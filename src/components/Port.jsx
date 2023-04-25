3aa41a7ecece644261eda2626c0c619cbf53f187
YT Choong <e2choong@yahoo.com>
2023-04-25 17:20:54 +0800

    updated to 6 showcase

--- src/components/Port.jsx
+++ src/components/Port.jsx
@@ -1,57 +1,74 @@
 import React from "react";
-import img1 from "../assets/port1.png";
+import img1 from "../assets/port1.jpg";
 import img2 from "../assets/port2.jpg";
 import img3 from "../assets/port3.jpg";
+import img4 from "../assets/port4.jpg";
+import img5 from "../assets/port5.jpg";
+import img6 from "../assets/port6.jpg";
 
 const portItem = [
   {
     id: 1,
     img: img1,
-    title: "How to Create Your Own Website",
-    text: "Lorem ipsum dolor sit, amet consectetur adipisicing elit.Doloribus natus voluptas, eos obcaecati recusandae amet?",
+    title: "Building a neat-looking portfolio website",
+    text: "Create a desktop and mobile version website from Adobe XD. Use some cold css styling with a bit of Javascript",
     url: "https://itosan2.github.io/portfolio/",
   },
   {
     id: 2,
     img: img2,
-    title: "How to Create Your Own Website",
-    text: "Lorem ipsum dolor sit, amet consectetur adipisicing elit.Doloribus natus voluptas, eos obcaecati recusandae amet?",
-    url: "#",
+    title: "Relax with a game of Tenzies",
+    text: "Tenzies game created using React. Replacing the die face with dots",
+    url: "https://itosan2.github.io/tenzies",
   },
   {
     id: 3,
     img: img3,
-    title: "How to Create Your Own Website",
-    text: "Lorem ipsum dolor sit, amet consectetur adipisicing elit.Doloribus natus voluptas, eos obcaecati recusandae amet?",
-    url: "#",
+    title: "AirBnB Experiences Clone",
+    text: "Creating a Dynamic Image Gallery with React: Insights from Mr. Bob Ziroll's Expertise",
+    url: "https://itosan2.github.io/abnb-tut",
   },
   {
     id: 4,
-    img: img1,
-    title: "How to Create Your Own Website",
-    text: "Lorem ipsum dolor sit, amet consectetur adipisicing elit.Doloribus natus voluptas, eos obcaecati recusandae amet?",
-    url: "#",
+    img: img4,
+    title: "Morning Night Toggle",
+    text: "Toggle between morning and night mode changing the image and greeting text",
+    url: "https://itosan2.github.io/morning/",
+  },
+  {
+    id: 5,
+    img: img5,
+    title: "Tic Tac Toe migration",
+    text: "This is a original application before migrating to React with TypeScript.",
+    url: "https://itosan2.github.io/tictactoe2/",
+  },
+  {
+    id: 6,
+    img: img6,
+    title: "Meme Generation",
+    text: "React apps fectching a random meme from api.imgflip.com",
+    url: "https://itosan2.github.io/meme/",
   },
 ];
 
 export default function Port() {
   return (
-    <section className="section sec4" id="blogs">
-      <div className="blogs-content">
+    <section className="section sec4" id="ports">
+      <div className="ports-content">
         <div className="main-title">
           <h2>
             My <span>Portfolio</span>
             <span className="bg-text">Portfolio</span>
           </h2>
         </div>
-        <p className="port-text">Here is a selection of my works</p>
-        <div className="blogs">
+        <p className="port-para">Here is a selection of my works</p>
+        <div className="ports">
           {portItem.map((item) => {
             return (
-              <div key={item.id} className="blog">
+              <div key={item.id} className="port">
                 <a href={item.url} target="_blank">
-                  <img src={item.img} alt="image 1" />
-                  <div className="blog-text">
+                  <img src={item.img} alt={item.title} />
+                  <div className="port-text">
                     <h4>{item.title}</h4>
                     <p>{item.text}</p>
                   </div>
--- src/index.js
+++ src/index.js
@@ -5,7 +5,6 @@ import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <p>this main</p>
     <App />
   </React.StrictMode>
 );
