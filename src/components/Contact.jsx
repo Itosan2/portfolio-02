5a2200861f9056784e926fa34d193d27be46e6fb
YT Choong <e2choong@yahoo.com>
2023-05-24 22:34:07 +0800

    change contact design

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,6 +2,32 @@ import React, { useEffect, useState } from "react";
 // import FontChecker from "./FontChecker";
 
 export default function Contact() {
+  // const [firstName, setFirstName] = useState("");
+  // const [subject, setSubject] = useState("");
+  // const [email, setEmail] = useState("");
+  // const [message, setMessage] = useState("");
+  const [person, setPerson] = useState({
+    firstName: "",
+    email: "",
+    subject: "",
+    textarea: "",
+  });
+  const [people, setPeople] = useState([]);
+
+  const handleChange = (e) => {
+    const name = e.target.name;
+    const value = e.target.value;
+    setPerson({ ...person, [name]: value });
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (person.firstName && person.email && person.subject) {
+      const newPerson = { ...person, id: new Date().getTime().toString() };
+      setPeople([...people, newPerson]);
+      setPerson({ firstName: "", email: "", subject: "", textarea: "" });
+    }
+  };
+
   return (
     <section className="section sec5 contact" id="contact">
       <div className="contact-container">
@@ -13,7 +39,7 @@ export default function Contact() {
         </div>
         <div className="contact-content-con">
           <div className="left-contact">
-            <h4>Contact me here</h4>
+            <h4>Contact Information</h4>
             <p>
               Lorem ipsum dolor sit amet consectetur adipisicing elit. In,
               laborum numquam? Quam excepturi perspiciatis quas quasi.
@@ -59,32 +85,67 @@ export default function Contact() {
               </div>
             </div>
           </div>
+
           <div className="right-contact">
-            <form action="" className="contact-form">
-              <div className="input-control i-c-2">
-                <input type="text" required placeholder="YOUR NAME" />
-                <input type="email" required placeholder="YOUR EMAIL" />
+            <form
+              className="contact-form"
+              action="contactform.php"
+              method="post"
+              onSubmit={handleSubmit}
+            >
+              <h2>Message Me</h2>
+              <div className="inputBox">
+                <input
+                  type="text"
+                  required
+                  id="firstName"
+                  name="firstName"
+                  value={person.firstName}
+                  onChange={handleChange}
+                  placeholder=""
+                />
+                <span>Your Name</span>
               </div>
-              <div className="input-control">
-                <input type="text" required placeholder="ENTER SUBJECT" />
+              <div className="inputBox">
+                <input
+                  type="email"
+                  required
+                  placeholder=""
+                  id="email"
+                  name="email"
+                  value={person.email}
+                  onChange={handleChange}
+                />
+                <span>Email</span>
               </div>
-              <div className="input-control">
+              <div className="inputBox">
+                <input
+                  className="inputBox"
+                  type="text"
+                  required
+                  id="subject"
+                  name="subject"
+                  value={person.subject}
+                  onChange={handleChange}
+                />
+                <span>Subject</span>
+              </div>
+              <div className="inputBox">
                 <textarea
-                  name=""
-                  id=""
+                  className="inputBox"
+                  type="textarea"
+                  name="textarea"
+                  id="textarea"
+                  value={person.textarea}
+                  onChange={handleChange}
                   cols="15"
-                  rows="6"
-                  placeholder="Message Here..."
+                  rows="4"
                 ></textarea>
+                <span>Message</span>
               </div>
-              <div className="submit-btn">
-                <a href="#" className="main-btn">
-                  <span className="btn-text">Submit</span>
-                  <span className="btn-icon">
-                    <i className="fas fa-envelope-open"></i>
-                  </span>
-                </a>
-              </div>
+              <button type="submit" className="submit-btn">
+                Submit
+              </button>
             </form>
           </div>
         </div>
